d38d4f342b3b6137c87a216d092a54a3ad90fe8f
julien <>
2012-11-12 17:45:23 +0100

    premier commit de test

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<form method="post" action="../controleurs/controleur_login">
+    <fieldset>
+        <label for="login">e-mail :</label>
+        <input type="text" name="login" id="login"></input><br/>
+        <label for="mdp">mot de passe :</label>
+        <input type="password" name="mdp" id="mdp"></input><br/>
+        <input type="submit" value="Valider" ></input>
+    </fieldset>
+</form>
+
 <?php
 
 echo "salut !"
