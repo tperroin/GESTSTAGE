88855e5b3641f589d8984575d982eb57c60dd071
Tibo <>
2012-11-13 20:50:55 +0100

    Début de mise en place de la gestion pour l'admin, vues et classes pour creer un utilisateur mises en place. Il reste à les définir

--- controleurs/C_Administrateur.class.php
+++ controleurs/C_Administrateur.class.php
@@ -1,7 +1,19 @@
 <?php
 
 class C_Administrateur extends Controleur{
-    
+    function creerUtilisateur(){
+        $this->vue->titreVue = 'Création d\'un utilisateur';   
+        
+        $this->vue->entete = "../vues/templates/entete.inc.php"; 
+        
+        $this->vue->gauche = "../vues/templates/gauche.inc.php";
+       
+        $this->vue->centre = "../vues/administrateur/templates/centre.creerUtilisateur.inc.php";
+        
+        $this->vue->pied = "../vues/templates/pied.inc.php";
+        
+        $this->vue->afficher();
+    }
 }
 
 ?>
---
+++ vues/administrateur/templates/centre.creerUtilisateur.inc.php
@@ -0,0 +1,8 @@
+<!-- VARIABLES NECESSAIRES -->
+<!-- $this->message : à afficher sous le formulaire -->
+<h3>Cr&eacute;ation d'un utilisateur</h3>
+<?php
+if (isset($this->message)) {
+    echo "<strong>" . $this->message . "</strong>";
+}
+?>
