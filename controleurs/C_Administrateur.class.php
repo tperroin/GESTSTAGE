e9e55ee32ff25f0cec72637032641c7d27863a54
Tibo <>
2012-11-19 15:44:41 +0100

    Modification script SQL.
    Création de l'utilisateur en place, reste à voir pour les id -> libellé

--- controleurs/C_Administrateur.class.php
+++ controleurs/C_Administrateur.class.php
@@ -39,23 +39,28 @@ class C_Administrateur extends Controleur{
         $lesParametres = array();
         // récupérer les données du formulaire
         $lesParametres[0] = 1;
-        var_dump($_POST);
-        $lesParametres[1] = $_POST['login'];
-        $lesParametres[2] = $_POST['mdp'];
-        $lesParametres[3] = $_POST['nom'];
-        $lesParametres[4] = $_POST['tel'];
-        $lesParametres[5] = $_POST['mail'];
-        $lesParametres[6] = $_POST['prenom'];
+        $lesParametres[1] = $_POST["login"];
+        $lesParametres[2] = $_POST["mdp"];
+        $lesParametres[3] = $_POST["nom"];
+        $lesParametres[4] = $_POST["tel"];
+        $lesParametres[5] = $_POST["mail"];
+        $lesParametres[6] = $_POST["prenom"];
         $lesParametres[7] = 'Mr';
-        $lesParametres[8] = 1;
-        $lesParametres[9] = 1;
+        $lesParametres[8] = 'aa';
+        $lesParametres[9] = 'aa';
         $lesParametres[10] = 1;
+        
+        foreach ($lesParametres as $value) {
+            echo $value .'          ';
+        }
+        
         $ok = $utilisateur->insert($lesParametres);
         if ($ok) {
             $this->vue->message = "Utilisateur cr&eacute;&eacute;";
         } else {
             $this->vue->message = "Echec de l'ajout de l'utilisateur";
         }
+        echo $ok;
         $this->vue->afficher();
     }
     
