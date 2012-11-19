c13f5d04653bf33984983443c2433a70db3072e8
ymachouri <>
2012-11-19 17:49:03 +0100

    19/11/2012 - Page de consultation crées prenant modèle sur la création d'un utilisateur. Il reste à relier avec la base de donénes et sortir les résultats et voir avec l'id de session.

--- controleurs/C_Utilisateur.class.php
+++ controleurs/C_Utilisateur.class.php
@@ -1,7 +1,31 @@
 <?php
 
 class C_Utilisateur extends Controleur{
-    
+    function coordonees(){
+        
+                $this->vue->titreVue = 'Cr&eacute;ation d\'un utilisateur';   
+        
+        $this->vue->entete = "../vues/templates/entete.inc.php"; 
+                
+        $this->vue->gauche = "../vues/templates/gauche.inc.php";
+        
+        $lesFormations = new M_ListeFormations();
+        $this->vue->lesFormations = $lesFormations->getAll();
+        
+        $lesClasses = new M_ListeClasses();
+        $this->vue->lesClasses = $lesClasses->getAll();
+        
+        $lesOptions = new M_ListeOptions();
+        $this->vue->lesOptions = $lesOptions->getAll();
+        
+        $lesRoles = new M_ListeRoles();
+        $this->vue->lesRoles = $lesRoles->getAll();
+       
+        $this->vue->centre = "../vues/utilisateur/templates/centre.affichermesInformations.inc.php";
+        
+        $this->vue->pied = "../vues/templates/pied.inc.php";
+        
+        $this->vue->afficher();
+    }
 }
-
 ?>
