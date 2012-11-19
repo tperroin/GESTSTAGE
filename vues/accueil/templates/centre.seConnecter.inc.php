4c7219e88825047058db6e3eb091b2acbeab96c5
TAKUUU <>
2012-11-19 17:30:00 +0100

    modification CSS

--- vues/accueil/templates/centre.seConnecter.inc.php
+++ vues/accueil/templates/centre.seConnecter.inc.php
@@ -6,11 +6,11 @@
         <input type="text" name="login" id="login"></input><br/>
         <label for="mdp">mot de passe :</label>
         <input type="password" name="mdp" id="mdp"></input><br/>
-        <input type="submit" value="Valider" ></input>
-    </fieldset>
-</form>
+        <input type="submit" value="Valider" ></input><br/>
 <?php
 if (isset($this->message)) {
     echo "<strong>".$this->message."</strong>";
 }
 ?>
+    </fieldset>
+</form>
